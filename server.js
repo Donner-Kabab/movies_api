02b198196cf70c95a119f98bc1f3b30514c0fda7
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-02-23 10:36:16 -0700

    Update server.js
    Fixed errors and added modules needed

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
+const http = require("http"),
+const fs = require("fs"),
 const url = require("url");
-let addr = request.url;
-let q = new URL(addr, "documentation");
-
-const http = require("http");
 
 http
-  .createServer((request, responst) => {
-    Response.writeHead(200, { "Content-Type": "text/plain" });
-    Response.end("Hello Node!");
+  .createServer((request, response) => {
+  let addr = request.url;
+  let q = new URL(addr, "http://localhost:8080");
+  filePath = "";
+
+  if (q.pathname.includes("documentation")) {
+    filePath = (_dirname + "/documentation.html");
+  } else {
+    filePath = "index.html";
+  }
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(data);
+      response.end();
+    });
   })
   .listen(8080);
-
-const fs = require("fs");
-
-fs.readFile("input.txt", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log("File content: " + data.toString());
-});
+  console.log("Server is running on Port 8080.");
