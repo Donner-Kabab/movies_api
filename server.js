aab4990e7c05e73d8975a2de031884f4bd2b27ee
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-02-24 21:23:02 -0700

    Updated html, txt, and js
    updated errors in code

--- server.js
+++ server.js
@@ -1,29 +1,4 @@
 const http = require("http"),
-<<<<<<< Updated upstream
-const fs = require("fs"),
-const url = require("url");
-
-http
-  .createServer((request, response) => {
-  let addr = request.url;
-  let q = new URL(addr, "http://localhost:8080");
-  filePath = "";
-
-  
-  fs.appendFile("log.txt", "URL: " = addr = "/nTimestamp: " = newDate() = "/n/n", (err) +> {
-    if (err) {
-      consoleconsole.log(err);
-    } else {
-      console.log("Added to log.");
-    }
-  });
-
-  if (q.pathname.includes("documentation")) {
-    filePath = (_dirname + "/documentation.html");
-  } else {
-    filePath = "index.html";
-  }
-=======
   fs = require("fs"),
   url = require("url");
 
@@ -50,23 +25,17 @@ http
     } else {
       filePath = "index.html";
     }
->>>>>>> Stashed changes
+
     fs.readFile(filePath, (err, data) => {
       if (err) {
         throw err;
       }
-<<<<<<< Updated upstream
-      response.writeHead(200, {"Content-Type": "text/html"});
-=======
+
       response.writeHead(200, { "Content-Type": "text/html" });
->>>>>>> Stashed changes
       response.write(data);
       response.end();
     });
   })
   .listen(8080);
-<<<<<<< Updated upstream
-  console.log("Server is running on Port 8080.");
-=======
+
 console.log("Server is running on Port 8080.");
->>>>>>> Stashed changes
