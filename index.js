540ac05576fb5c50d7f9b5a961f03f46439069cc
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-03-19 18:51:39 -0600

    fixed errors in Index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Models = require("./models.js");
 
 const Movies = Models.Movie;
 const Users = Models.User;
+const Directors = Models.Director;
 
 mongoose.connect("mongodb://localhost:27017/test");
 
@@ -103,27 +104,27 @@ app.post("/users/:Username/movies/:MovieID", async (req, res) => {
     });
 });
 
-//Delete remove movie title ****
-app.delete("/users/:id/:moveTitle", (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(
-      (title) => title !== movieTitle
-    );
-    res
-      .status(200)
-      .send("${movieTitle} has been removed from user ${id}'s array");
-  } else {
-    res.status(400).send("No such user");
-  }
+//Delete remove movie from favorites
+app.post("/users/:Username/movies/:MovieID", async (req, res) => {
+  await Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }
+  ) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
 });
 
 // Delete a user by username
 app.delete("/users/:Username", async (req, res) => {
-  await Users.findOneAndRemove({ Username: req.params.Username })
+  await Users.findOneAndDelete({ Username: req.params.Username })
     .then((user) => {
       if (!user) {
         res.status(400).send(req.params.Username + " was not found");
@@ -144,7 +145,7 @@ app.get("/movies", (req, res) => {
 
 // Get a movie by title
 app.get("/movies/:title", async (req, res) => {
-  await Movies.findOne({ title: req.params.title })
+  await Movies.findOne({ Title: req.params.title })
     .then((movies) => {
       res.json(movies);
     })
@@ -155,10 +156,10 @@ app.get("/movies/:title", async (req, res) => {
 });
 
 //read genre by name
-app.get("/movie/genre/:genreName", async (req, res) => {
-  await Movies.findOne({ genre: req.params.genreName })
-    .then((movies) => {
-      res.json(movies);
+app.get("/movies/genre/:genreName", async (req, res) => {
+  await Movies.findOne({ "Genre.Name": req.params.genreName })
+    .then((movie) => {
+      res.json(movie.Genre);
     })
     .catch((err) => {
       console.error(err);
@@ -167,20 +168,15 @@ app.get("/movie/genre/:genreName", async (req, res) => {
 });
 
 //read director
-app.get("/directors/:directorName", async (req, res) => {
-  await Director.findOne({ name: req.params.directorName })
-    .then((directors) => {
-      res.json(directors);
+app.get("/movies/director/:directorName", async (req, res) => {
+  await Movies.findOne({ "Director.Name": req.params.directorName })
+    .then((movie) => {
+      res.json(movie.Director);
     })
     .catch((err) => {
       console.error(err);
       res.status(500).send("Error: " + err);
     });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke");
 });
 
 //Listen for requests
--- models.js
+++ models.js
@@ -29,10 +29,18 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }], //reference
 });
 
+let directorSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Bio: { type: String, required: true },
+  Birth: { type: Date, required: true },
+});
+
 //after defining your schemas, create models to use the defined schemas
 let Movie = mongoose.model("Movie", movieSchema);
 let User = mongoose.model("User", userSchema);
+let Director = mongoose.model("Directors", directorSchema);
 
 //exports modules so we can import to index.js
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Director = Director;
