46a9e724f769ec5aad3279a8660f176d97b5cfc5
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-03-25 14:22:01 -0600

    msg

--- passport.js
+++ passport.js
@@ -24,7 +24,7 @@ passport.use(
               message: "Incorrect username or password.",
             });
           }
-          if (!user.validatePassword(passsword)) {
+          if (!user.validatePassword(password)) {
             console.log("incorrect password");
             return callback(null, false, { message: "Incorrect password." });
           }
